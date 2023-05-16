7f1dab8d89014d14f403a35e6047a99c470c235d
Joseph Rajewski <83741749+paladin-devops@users.noreply.github.com>
2023-05-16 11:27:12 -0400

    Fix role input on iam role policy attachment.

--- modules/ci/main.tf
+++ modules/ci/main.tf
@@ -82,6 +82,5 @@ EOF
 
 resource "aws_iam_role_policy_attachment" "github_actions_role_policy_attachment" {
   policy_arn = aws_iam_policy.github_actions_policy_ecr.arn
-  role       = aws_iam_role.github_actions_role
-
+  role       = aws_iam_role.github_actions_role.name
 }
