0db6135e83aa29241642cec0358b833a41334f56
Joseph Rajewski <83741749+paladin-devops@users.noreply.github.com>
2023-06-02 18:24:18 -0400

    Remove invalid type field for generic secret for telemetry.

--- modules/telemetry/main.tf
+++ modules/telemetry/main.tf
@@ -16,14 +16,12 @@ resource "datadog_dashboard" "app_metrics" {
 
 resource "vault_generic_secret" "dev_datadog_key" {
   provider  = vault.dev
-  type      = "kv-v2"
   data_json = "{\"api_key\": \"${var.datadog_api_key}\"}"
   path      = "${var.vault_dev_kv_secrets_engine_path}/datadog"
 }
 
 resource "vault_generic_secret" "prod_datadog_key" {
   provider  = vault.prod
-  type      = "kv-v2"
   data_json = "{\"api_key\": \"${var.datadog_api_key}\"}"
   path      = "${var.vault_prod_kv_secrets_engine_path}/datadog"
 }
