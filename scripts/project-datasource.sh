bfd06bd4ba6204696f6003c657dc013278ba280f
Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>
2023-07-07 09:24:10 -0400

    Skipping apt install prompt

--- scripts/project-datasource.sh
+++ scripts/project-datasource.sh
@@ -40,7 +40,7 @@ fi
 # Install the latest Waypoint CLI
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install waypoint
+sudo apt update && sudo apt -y install waypoint
 
 # Create a CLI context to hook up to the HCP Waypoint server
 waypoint context create \
